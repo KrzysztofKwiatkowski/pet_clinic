6966d3098a65f63637259c6bcb8462f1a1e38af1
Krzysztof Kwiatkowski <bobo2000@wp.pl>
2020-01-11 17:07:20 +0100

    POJOs. Closes #2.

--- src/main/java/com/kkwiatkowski/model/Owner.java
+++ src/main/java/com/kkwiatkowski/model/Owner.java
@@ -5,4 +5,5 @@ public class Owner extends Person {
 
 
 
+
 }
--- src/main/java/com/kkwiatkowski/model/Pet.java
+++ src/main/java/com/kkwiatkowski/model/Pet.java
@@ -8,6 +8,7 @@ public class Pet {
     private Owner owner;
     private LocalDate birthDate;
 
+
     public PetType getPetType() {
         return petType;
     }
